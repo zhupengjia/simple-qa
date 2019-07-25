28b2948ae14831d3edead091b33dbff59cc18f0d
Pengjia Zhu <zhupengjia@gmail.com>
2019-07-26 02:08:50 +1200

    updated

--- xlnet_qa/qa_server.py
+++ xlnet_qa/qa_server.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
-
+from pytorch_transformers import BERTTokenizer
 
 class QAServer:
     """
         QA restful server
     """
-    def __init__(self, url_rule="/api/query", port=5002):
-        pass
+    def __init__(self, tokenizer_name="bert-base-uncased", url_rule="/api/query", port=5002):
+        self.tokenizer = BERTTokenizer.from_pretrained(tokenizer_name)
+        self.vocab =self.tokenizer.vocab
+
+    def build_index(self, ziptxt):
+        """
+            Build index
+
+            Input:
+                - ziptxt: compressed txt file via gzip
+        """
+        with gzip.open(ziptxt) as f:
+            pass
+
 
 
