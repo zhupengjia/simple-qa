b4937584c052acd150b8468ff53bd43d5b31cffb
Pengjia Zhu <zhupengjia@gmail.com>
2019-07-29 01:17:25 +1200

    updated qa_server

---
+++ interact.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from .xlnet_qa.qa_server.py import QAServer
+from pytorch_transformers import XLNetTokenizer
+
+tokenizer = XLNetTokenizer.from_pretrained("xlnet-large-cased")
+s = QAServer("data/wiki/wiki.bz2")
+
+
--- xlnet_qa/qa_server.py
+++ xlnet_qa/qa_server.py
@@ -1,39 +1,68 @@
 #!/usr/bin/env python
+import plyvel, tqdm
 from nltk import sent_tokenize
 from nlptools.text import TFIDF
-from nlptools.utils import zload, zdump
-import plyvel
+from nlptools.utils import zloads, zdumps
 
 class QAServer:
     """
         QA restful server
     """
-    def __init__(self, ziptxt, tokenizer, cached_index, url_rule="/api/query", port=5002):
+    def __init__(self, filepath, tokenizer, url_rule="/api/query", port=5002):
         """
             Input:
-                - ziptxt: compressed txt file via gzip
+                - filepath: txt file path, support .gzip, .bzip2, and .txt file
                 - tokenizer: tokenizer from pytorch_transformers
         """
         self.tokenizer = tokenizer
-        self.tfidf = TFIDF(vocab_size=self.tokenizer.vocab_size, cached_index=cached_index)
-        self.cached_contents = cached_index + ".contents"
+        self.tfidf = None
+        self.build_db(filepath)
+        self.build_index(filepath)
 
-    def build_index(self, ziptxt):
+    def build_db(self, filepath):
         """
-            Build index
+            save txt to LevelDB
 
             Input:
-                - ziptxt: compressed txt file via gzip
+                - filepath: txt file path, support .gzip, .bzip2, and .txt file
         """
-        if os.path.exists(self.cached_contents):
+        cached_contents = filepath + ".contents"
+        if os.path.exists(cached_contents):
             return
-        self.contents = plyvel.DB(self.cached_contents, create_if_missing=True)
-        num_lines = sum(1 for line in gzip.open(ziptxt))
-        with gzip.open(ziptxt) as f:
+        self.contents = plyvel.DB(cached_contents, create_if_missing=True)
+        ext = os.path.splitext(filepath)
+
+        if ext == ".gz":
+            import gzip
+            open_func = gzip.open
+        elif ext == ".bz2":
+            open_func = bz2.open
+        else:
+            open_func = open
+
+        with open_func(filepath) as f, self.contents.write_batch() as wb:
+            totN = 0
             for i, l in enumerate(f):
                 for j, sent in enumerate(sent_tokenize(l)):
                     token_ids = self.tokenizer.encode(j)
+                    wb.put(bytes(totN), zdumps({"sentence": sent, "ids": token_ids}))
+                    totN += 1
+            wb.put(b"total", zdumps({"N": totN, "Ndocs": i+1}))
 
+    def build_index(self, filepath)
+        """
+            build index
 
+            Input:
+                - filepath: txt file path, support .gzip, .bzip2, and .txt file
+        """
+        cached_index = filepath + '.index'
+        self.tfidf = TFIDF(vocab_size=self.tokenizer.vocab_size, cached_index=cached_index)
+        totN = zloads(self.contents.get(b'total'))["N"]
+        def data_iter():
+            with self.contents.snapshot() as sn:
+                for i in range(totN):
+                    yield zloads(sn.get(bytes(i)))["ids"]
+        self.tfidf.load_index(corpus_ids=data_iter, corpus_len=totN)
 
 
