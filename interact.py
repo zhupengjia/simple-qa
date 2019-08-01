258bee9baf782dc2f0e3361a48dbb02dae37a697
Pengjia Zhu <zhupengjia@gmail.com>
2019-08-01 15:32:34 +1200

    updated for using whoose instead

--- interact.py
+++ interact.py
@@ -4,8 +4,6 @@ import argparse
 parser = argparse.ArgumentParser(description='Read comprehension using XLNet pretrained model')
 parser.add_argument('--device', dest="device", default="cuda:0", help="choose to use cpu or cuda:x, default is cuda:0")
 parser.add_argument('--recreate', dest="recreate", action='store_true', help="set to recreate index")
-parser.add_argument('--before', dest="before", default=0, help="prefiltered texts include N lines before related text, default is 0")
-parser.add_argument('--after', dest="after", default=0, help="prefiltered texts include N lines after related text, default is 0")
 parser.add_argument('-p', '--port', dest='port', default=5002, help="listen port, default is 5002")
 parser.add_argument('--backend', dest='backend', default='shell', help="choose for backend from: shell, restful, default is shell")
 parser.add_argument('-m', '--model', dest="model", required=True, help="path of model, must be a directory and contains file 'pytorch_model.bin' and 'config.json'")
--- xlnet_qa/qa_server.py
+++ xlnet_qa/qa_server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-import os, sqlite3, nltk, string, torch
+import os, nltk, string, torch, shutil
 from tqdm import tqdm
-from nlptools.text import TFIDF
-from nlptools.utils import lloads, ldumps
 from pytorch_transformers import XLNetForQuestionAnswering
+from whoosh.analysis import StemmingAnalyzer
+from whoosh.fields import TEXT, Schema
+from whoosh import qparser, index
 from .squad2_reader import SQuAD2Reader
 
 class QAServer:
@@ -32,18 +33,13 @@ class QAServer:
                                   doc_stride = doc_stride,
                                   max_query_len = max_query_len,
                                   is_training = False)
-        self.tokenizer = self.reader.tokenizer
-        self.vocab_size = self.tokenizer.vocab_size
         if not torch.cuda.is_available():
             self.device = "cpu"
         else:
             self.device = device
 
         self.translator = str.maketrans('', '', string.punctuation) # remove punctuation
-       
-        self.tfidf = None
-        self.cached_contents = file_path + ".db"
-        self._build_db(file_path, recreate)
+
         self._build_index(file_path, recreate)
         self._load_model(model_path, self.device)
 
@@ -52,7 +48,7 @@ class QAServer:
         cursor = db.cursor()
         return db, cursor
 
-    def _build_db(self, filepath, recreate=False):
+    def _build_index(self, filepath, recreate=False):
         """
             save txt to LevelDB
 
@@ -60,142 +56,76 @@ class QAServer:
                 - filepath: txt file path, support .gzip, .bzip2, and .txt file
                 - recreate: bool, True will force recreate db, default is False
         """
-        cached_contents = filepath + ".db"
+        cached_index = filepath + ".index"
 
-        if os.path.exists(cached_contents):
+        if os.path.exists(cached_index):
             if recreate:
-                os.remove(cached_contents)
+                shutil.rmtree(cached_index)
         else:
             recreate = True
 
-        db, cursor = self._get_cursor(cached_contents)
+        stem_ana = StemmingAnalyzer()
+        self.schema = Schema(content=TEXT(stored=True, analyzer=stem_ana))
+
         if not recreate:
-            totN = list(cursor.execute("select max(id) from contents"))[0][0]
-            if totN is not None:
-                db.close()
-                return
-
-        cursor.execute('''create table if not exists contents (
-            id integer primary key,
-            pid integer,
-            sid integer,
-            sentence blob,
-            token_ids blob)''')
-
-        ext = os.path.splitext(filepath)[-1]
-        if ext == ".bz2":
-            import bz2
-            open_func = bz2.open
-        elif ext == ".gz":
-            import gzip
-            open_func = gzip.open
+            self.ix = index.open_dir(cached_index, indexname="qa_content")
         else:
-            open_func = open
+            os.makedirs(cached_index)
+            self.ix = index.create_in(cached_index, self.schema, indexname="qa_content")
 
-        with open_func(filepath, mode="rt", encoding="utf-8") as f:
-            totN, totP, totS= 0, 0, 0
-            batch_data = []
-            for l in tqdm(f, desc="Converting to sqlite", unit=" lines"):
-                l = l.strip()
-                if len(l) < 1 :
-                    if totS > 0 : totP += 1
-                    totS = 0
-                for sent in nltk.sent_tokenize(l):
-                    sent = sent.strip()
-                    clean_sent = sent.translate(self.translator).lower().strip()
-                    if len(clean_sent) < 1:
+            ext = os.path.splitext(filepath)[-1]
+            if ext == ".bz2":
+                import bz2
+                open_func = bz2.open
+            elif ext == ".gz":
+                import gzip
+                open_func = gzip.open
+            else:
+                open_func = open
+
+            with open_func(filepath, mode="rt", encoding="utf-8") as f:
+                totN, totP, totS= 0, 0, 0
+                writer = self.ix.writer()
+                for l in tqdm(f, desc="Building index", unit=" lines"):
+                    l = l.strip()
+                    if len(l) < 1 :
+                        if totS > 0 : totP += 1
+                        totS = 0
                         continue
-                    token_ids = self.tokenizer.encode(clean_sent)
-
-                    if len(token_ids) < 1:
-                        continue
-                    batch_data.append((totN, totP, totS, ldumps(sent), ldumps(token_ids)))
-                    totN += 1
-                    totS += 1
-                    if totN % 100000 == 0:
-                        cursor.executemany("""insert into contents values (?,?,?,?,?)""", batch_data)
-                        db.commit()
-                        batch_data = []
-            if len(batch_data) > 0:
-                cursor.executemany("""insert into contents values (?,?,?,?,?)""", batch_data)
-            db.commit()
-        db.close()
-
-    def _build_index(self, filepath, recreate=False):
-        """
-            build index
-
-            Input:
-                - filepath: txt file path, support .gzip, .bzip2, and .txt file
-                - recreate: bool, True will force recreate db, default is False
-        """
-        cached_index = filepath + '.index'
-        cached_contents = filepath + ".db"
-
-        self.tfidf = TFIDF(vocab_size=self.vocab_size, cached_index=cached_index)
-
-        db, cursor = self._get_cursor(cached_contents)
-        totN = list(cursor.execute("select max(id) from contents"))[0][0] + 1
-        db.close()
-
-        def data_iter():
-            db, cursor = self._get_cursor(cached_contents)
-            for data in cursor.execute('select * from contents order by id'):
-                yield lloads(data[4])
-            db.close()
-        self.tfidf.load_index(corpus_ids=data_iter(), corpus_len=totN, retrain=recreate)
+                    for sent in nltk.sent_tokenize(l):
+                        sent = sent.strip()
+                        clean_sent = sent.translate(self.translator).lower().strip()
+                        if len(clean_sent) < 1:
+                            continue
+                        writer.add_document(content=clean_sent)
+                        totN += 1
+                        totS += 1
+                writer.commit()
+        
+        og = qparser.OrGroup.factory(0.9)
+        self.parser = qparser.QueryParser("content", schema=ix.schema, group=og)
+        self.parser.add_plugin(qparser.FuzzyTermPlugin())
 
     def _load_model(self, model_path, device):
         self.model = XLNetForQuestionAnswering.from_pretrained(model_path)
         self.model.to(device)
         self.model.eval()
 
-    def search(self, text, topN=5, before=0, after=0):
+    def search(self, text, topN=5):
         """
             search related text
 
             Input:
                 - text: string
                 - topN: int, return topN results, default is 5
-                - before: int, return Nlines before result, default is 0
-                - after: int, return Nlines after result, default is 0
         """
         text = text.translate(self.translator).lower().strip()
-        token_ids = self.tokenizer.encode(text)
-        if len(token_ids) < 1:
+        if len(text) < 1:
             return None
-        result = self.tfidf.search_index(token_ids, topN=topN)
-
-        db, cursor = self._get_cursor(self.cached_contents)
-        sql = "select pid, sentence from contents where id=?"
-
-        all_ids, all_sentences = [], []
-        for i in [r[0] for r in result]:
-            raw_pid, raw_sentence = list(cursor.execute(sql, (i,)))[0]
-
-            ids, sentences = [], []
-            for j in range(before, 0, -1):
-                pid, sentence = list(cursor.execute(sql, (i-j,)))[0]
-                if pid != raw_pid:
-                    continue
-                ids.append(i-j)
-                sentences.append(sentence)
-            ids.append(i)
-            sentences.append(raw_sentence)
-            for j in range(after):
-                pid, sentence = list(cursor.execute(sql, (i+j+1,)))[0]
-                if pid != raw_pid:
-                    continue
-                ids.append(i+j+1)
-                sentences.append(sentence)
-            all_ids += ids
-            all_sentences += [lloads(s) for s in sentences]
-
-        db.close()
-
-        if len(all_ids) < 1:
-            return None
-        return all_sentences
+        query = parser.parse(text)
+        
+        results = searcher.search(query, limit=topN, terms=True)
+        return [r['content'] for r in results]
 
     def __call__(self, question):
         related_texts = self.search(question)
